defb740754891a582ba48336cc128d0c61737380
chenjiabin <>
2023-08-10 12:11:13 +0800

    移除领英

--- channel/linkedin/project_id/检查项目下的候选人.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("/Users/chenjiabin/Project/data-sync-cdc/channel/linkedin/project_id/un_repeat_project_map_candidate.jsonl") as f:
-    while line := f.readline():
-        data = json.loads(line)
-        if data["projectId"] == "432223697":
-            print(data)
--- channel/linkedin/script/get_resume.py
+++
@@ -1,5 +0,0 @@
-import json
-with open("/Users/chenjiabin/Project/data-sync-cdc/channel/linkedin/project_id/project_map_candidate.jsonl",'r') as f:
-    while line := f.readline():
-        print(json.loads(line))
-        break
